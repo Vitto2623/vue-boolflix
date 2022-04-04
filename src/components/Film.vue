894d23c1d19c9ca41513f57ac307cbee83ff922a
Vitto2623 <vtr.1@icloud.com>
2022-04-04 18:57:17 +0200

    Sistemo grandezza cards

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -4,27 +4,32 @@
             <img :src="getPoster(film.poster_path)" alt="">
             <h1>{{film.title}}</h1>
             <h2>{{film.original_title}}</h2>
-            <h3>{{film.original_language}}</h3>
+            <Lang-flag  :iso ="film.original_language" />
             <h3>{{film.vote_average}}</h3>
         </div>
-        <div class="my-card">
+        <div class="my-card m-5">
             <h1>{{film.original_name}}</h1>
             <h3>{{film.original_language}}</h3>
-            <h3>{{film.vote_average}}</h3>
+            <span v-for="(x, index) in Math.round(film.vote_average)" :key="index">*</span>
         </div>
     </div>
 </template>
 
 <script>
+import LangFlag from 'vue-lang-code-flags';
 export default {
     name : "IndexFilm",
     props: {
         "film" : Object
     },
+    components:{
+        LangFlag,
+    },
     methods:{
         getPoster(path){
             return `https://image.tmdb.org/t/p/w342/${path}`
-        }
+        },
+
     }
 }
 </script>
--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -24,7 +24,7 @@ export default {
     'seriesTv' : Array
   },
   components: {
-    Film
+    Film,
   },
 }
 </script>
